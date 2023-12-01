bff7182727d3a96d8e151235e57db2d3bac2959e
PillageDev <63556914+PillageDev@users.noreply.github.com>
2023-11-30 20:50:22 -0500

    Big UI overhaul
    Co-authored-by: rohanmishra <rohanmishr@users.noreply.github.com>
    Co-authored-by: Incbom <IncbomDev@users.noreply.github.com>

--- mobile/App1.js
+++ mobile/App1.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, ScrollView, LayoutAnimation } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, ScrollView, LayoutAnimation, Dimensions, Button } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { Image } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
@@ -8,6 +8,8 @@ import { Preloader } from './Preloader';
 import { useCallback } from "react";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import * as Font from 'expo-font';
+import { TouchableOpacity } from 'react-native';
 
 import {
     useResponsiveHeight,
@@ -15,46 +17,19 @@ import {
     useDimensionsChange,
     useResponsiveFontSize
 } from "react-native-responsive-dimensions";
-import { PanGestureHandler } from 'react-native-gesture-handler';
-
-const onSwipePerformed = (action) => {
-    /// action : 'left' for left swipe
-    /// action : 'right' for right swipe
-    /// action : 'up' for up swipe
-    /// action : 'down' for down swipe
-    
-    switch(action){
-      case 'left':{
-        console.log('left Swipe performed');
-        break;
-      }
-       case 'right':{
-        console.log('right Swipe performed');
-        break;
-      }
-       case 'up':{
-        console.log('up Swipe performed');
-        break;
-      }
-       case 'down':{
-        console.log('down Swipe performed');
-        break;
-      }
-       default : {
-       console.log('Undeteceted action');
-       }
-    }
-}
-
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+        
 const App1 = ({ navigation }) => {
-    const height = useResponsiveHeight(25);
-    const width = useResponsiveWidth(25);
+    const { height, width } = Dimensions.get('window');
 
-    useDimensionsChange(
-        useCallback(({ window, screen }) => {
-        LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
-        }, [])
-    );
+    function percnetHeight(percent) {
+        return (percent / 100) * height;
+    }
+
+    function percentWidth(percent) {
+        return (percent / 100) * width;
+    }
+
     /*const [fonts] = useFonts({
         'Product Sans': require('./assets/fonts/ProductSans.ttf')
     });*/
@@ -76,39 +51,66 @@ const App1 = ({ navigation }) => {
     }
 
     return (
-        <ScrollView style={{border: '1px red solid'}}>
-            <PanGestureHandler onSwipePerformed={onSwipePerformed} gestureStyle={styles.swipesGestureContainer}>
-                <View style={{ height: height, width: width }}>
-                    <View style={{ height: percentHeight(20), width: percentWidth(100), paddingTop: percentHeight(10) }}>
-                        <Text style={{ fontSize: percentFontSize(10), textAlign: 'center', fontFamily: 'Rounded Mplus 1c', color: '#7F7F7F', fontWeight: 400, position: 'absolute', top: percentHeight(12), paddingLeft: percentWidth(5) }}>Thursday</Text>
-                        <Text style={{ fontSize: percentFontSize(15), textAlign: 'center', fontFamily: 'Rounded Mplus 1c', color: '#040415', fontWeight: 900, position: 'absolute', top: percentHeight(15), paddingTop: percentHeight(7.5), paddingLeft: percentWidth(5) }}>12 October</Text>
-                        <Text style={{ fontSize: percentFontSize(17.5), textAlign: 'center', fontFamily: 'Rounded Mplus 1c', color: '#2F394B', fontWeight: 900, position: 'absolute', top: percentHeight(50), paddingLeft: percentWidth(3) }}>Hi, {user.fName}</Text>
-                        <Text style={{ fontSize: percentFontSize(10), textAlign: 'center', fontFamily: 'Rounded Mplus 1c', color: '#7F7F7F', fontWeight: 400, position: 'absolute', top: percentHeight(70), paddingLeft: percentWidth(3) }}>1 Competition Upcoming.</Text>
-                        <View style={{ fontSize: percentFontSize(10), textAlign: 'left', fontFamily: 'Rounded Mplus 1c', color: '#000000', fontWeight: 200, position: 'absolute', top: percentHeight(90), paddingTop: percentHeight(7.5), paddingLeft: percentWidth(3), left: percentWidth(3), width: percentWidth(94), height: percentHeight(40), backgroundColor: 'purple', borderRadius: percentHeight(7) }}>
-                            <Text style={{ color: 'black' }}>Montgomery</Text>
+        <GestureHandlerRootView>
+            <ScrollView style={{border: '1px red solid'}}>
+                {/* <PanGestureHandler onSwipePerformed={onSwipePerformed} gestureStyle={styles.swipesGestureContainer}> */}
+                    <View style={{ height: height, width: width }}>
+                        <View style={{ height: percentHeight(10), width: '100%', paddingTop: percentHeight(10) }}>
+                            <Text style={{ fontSize: percentFontSize(2.5), textAlign: 'center', fontFamily: 'Rounded Mplus 1c', color: '#7F7F7F', fontWeight: 400, position: 'absolute', top: percentHeight(12), paddingLeft: percentWidth(5) }}>Thursday</Text>
+                            <Text style={{ fontSize: percentFontSize(3), textAlign: 'center', fontFamily: 'Rounded Mplus 1c', color: '#040415', fontWeight: 900, position: 'absolute', top: percentHeight(15), paddingLeft: percentWidth(5) }}>12 October</Text>
+                            <TouchableOpacity onPress={() => navigation.navigate('Profile')} style={{ position: 'absolute', top: percentHeight(13.5), right: percentWidth(10) }}>
+                                <View style={{ width: percentWidth(6.5), height: percentWidth(6.5), borderRadius: percentWidth(6.5), backgroundColor: '#E5E5E5', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                                    <Icon name="search-outline" style={{ color: '#040415' }} size={percentWidth(4)} />
+                                </View>
+                            </TouchableOpacity>
+                            <Text style={{ fontSize: percentFontSize(3), textAlign: 'center', fontFamily: 'Rounded Mplus 1c', color: '#2F394B', fontWeight: 900, position: 'absolute', top: percentHeight(20), paddingLeft: percentWidth(3), paddingTop: percentHeight(2.5) }}>Hi, {user.fName}</Text>
+                            <Text style={{ fontSize: percentFontSize(2), textAlign: 'center', fontFamily: 'Rounded Mplus 1c', color: '#7F7F7F', fontWeight: 400, position: 'absolute', top: percentHeight(25), paddingLeft: percentWidth(3), paddingTop: percentHeight(1.5) }}>1 Competition Upcoming.</Text>
+                            <View style={{ width: percentWidth(100), height: percentHeight(10), display: 'flex', alignItems: 'center', position: 'absolute', top: percentHeight(31) }}>
+                                <LinearGradient 
+                                    colors={['#8B78FF', '#5451D6']}
+                                    start={{ x: 0, y: 0 }}
+                                    end={{ x: 1, y: 0 }}
+                                    style={{ width: percentWidth(90), height: percentHeight(10), borderRadius: percentHeight(1), display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+                                >
+                                    <TouchableOpacity onPress={() => navigation.navigate('Competition')} style={[{ width: '100%', height: '100%', display: 'flex', justifyContent: 'center', paddingLeft: percentWidth(2) }]}>
+                                        <Text style={{ fontSize: percentFontSize(2), textAlign: 'left', fontFamily: 'Rounded Mplus 1c', color: '#FFFFFF', fontWeight: 900 }}>Montgomery</Text>
+                                        <Text style={{ fontSize: percentFontSize(1.5), textAlign: 'left', fontFamily: 'Rounded Mplus 1c', color: '#fff', fontWeight: 400 }}>Starts in 2 days</Text>
+                                        <View style={{ flexDirection: 'row', justifyContent: 'left', alignItems: 'center' }}>
+                                            <Icon name="person-circle" style={{ width: percentWidth(3), height: percentWidth(3), borderRadius: percentWidth(3), zIndex: 1, color: 'white' }} size={percentWidth(3)} />
+                                            <Icon name="person-circle" style={{ width: percentWidth(3), height: percentWidth(3), borderRadius: percentWidth(3), marginLeft: percentWidth(-1.25), zIndex: 2, color: 'blue' }} size={percentWidth(3)} />
+                                            <Icon name="person-circle" style={{ width: percentWidth(3), height: percentWidth(3), borderRadius: percentWidth(3), marginLeft: percentWidth(-1.25), zIndex: 3, color: 'red' }} size={percentWidth(3)} />
+                                        </View>
+                                        <Icon name="chevron-forward-outline" style={{ position: 'absolute', right: percentWidth(2), color: 'white' }} size={percentWidth(3)} />
+                                    </TouchableOpacity>
+                                </LinearGradient>
+                            </View>
+                            <Text style={{ fontSize: percentFontSize(3), textAlign: 'center', fontFamily: 'Rounded Mplus 1c', color: '#040415', fontWeight: 900, position: 'absolute', top: percentHeight(40), paddingLeft: percentWidth(3), paddingTop: percentHeight(1.5) }}>Monthly Preview</Text>
+                            <View style={{ fontSize: percentFontSize(3), textAlign: 'left', fontFamily: 'Rounded Mplus 1c', color: '#000000', fontWeight: 200, position: 'absolute', top: percentHeight(90), paddingLeft: percentWidth(3), left: percentWidth(3), width: percentWidth(94), height: percentHeight(40), backgroundColor: 'purple', borderRadius: percentHeight(7) }}>
+                                <Text style={{ color: 'black' }}>Montgomery</Text>
+                            </View>
+                            <View style={{ height: percentHeight(1000 )}} />
                         </View>
-                        <View style={{ height: percentHeight(1000 )}} />
+                        <View style={{ 
+                            height: percentHeight(15),
+                            width: width,
+                            flexDirection: 'row',
+                            justifyContent: 'space-around',
+                            alignItems: 'center',
+                            position: 'fixed',
+                            marginTop: percentHeight(100),
+                            backgroundColor: '#fff',
+                            borderTopWidth: 1, 
+                            borderTopColor: '#ccc', 
+                        }}>
+                            <Icon name="home" />
+                            <Icon name="calendar-outline"/>
+                            <Icon name="chatbubble-ellipses-outline"/>
+                            <Icon name="person-circle-outline"/>
+                        </View>
                     </View>
-                    <View style={{ 
-                        height: percentHeight(15),
-                        width: width,
-                        flexDirection: 'row',
-                        justifyContent: 'space-around',
-                        alignItems: 'center',
-                        position: 'fixed',
-                        marginTop: percentHeight(100),
-                        backgroundColor: '#fff',
-                        borderTopWidth: 1, 
-                        borderTopColor: '#ccc', 
-                    }}>
-                        <Icon name="home" />
-                        <Icon name="calendar-outline"/>
-                        <Icon name="chatbubble-ellipses-outline"/>
-                        <Icon name="person-circle-outline"/>
-                    </View>
-                </View>
-            </PanGestureHandler>
-        </ScrollView>
+                {/* </PanGestureHandler> */}
+            </ScrollView>
+        </GestureHandlerRootView>
     );
 
 };
--- mobile/Preloader.js
+++ mobile/Preloader.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import './styles/preloader.css';
+import { StyleSheet } from 'react-native';
+// import './styles/preloader.css';
 
 class Preloader extends Component {
   componentDidMount() {
@@ -15,30 +16,247 @@ class Preloader extends Component {
 
   render() {
     return (
-      <main>
-        <div className="splash">
-          <div className="splash_logo">
-            SpikeScout
-          </div>
-          <div className="splash_svg">
-            <svg width="100%" height="100%">
-              <rect width="100%" height="100%"></rect>
-            </svg>
-          </div>
-          <div className="splash_minimize">
-            <svg width="100%" height="100%">
-              <rect width="100%" height="100%"></rect>
-            </svg>
-          </div>
-        </div>
-        <div className="text">
-          <p>Duis quis</p>
-          <p>nec sapien</p>
-          <button>More</button>
-        </div>
-      </main>
+      <View style={styles.body}>
+        <View style={styles.splash}>
+          <View style={styles.splash_logo}>
+            <Text style={styles.text}>SpikeScout</Text>
+          </View>
+          <View style={styles.splash_svg}>
+            <Svg width="100%" height="100%">
+              <Rect width="100%" height="100%" />
+            </Svg>
+          </View>
+          <View style={styles.splash_minimize}>
+            <Svg width="100%" height="100%">
+              <Rect width="100%" height="100%" />
+            </Svg>
+          </View>
+        </View>
+        <View style={styles.text}>
+          <Text style={styles['text p']}>Duis quis</Text>
+          <Text style={[styles['text p'], styles['text p:nth-child(2)']]}>nec sapien</Text>
+          <TouchableOpacity style={styles['text button']}>
+            <Text>More</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     );
   }
 }
 
+const styles = StyleSheet.create({
+  body: {
+    backgroundColor: '#191919',
+    fontFamily: 'Open Sans, sans-serif',
+  },
+  text: {
+    opacity: 0,
+    position: 'absolute',
+    zIndex: 7,
+    textAlign: 'left',
+    margin: -50,
+    width: 300,
+    height: 100,
+    top: '50%',
+    left: '50%',
+    fontSize: 48,
+    fontWeight: '700',
+    animationDuration: 4000,
+    animationFillMode: 'forwards',
+  },
+  'text p': {
+    whiteSpace: 'nowrap',
+    overflow: 'hidden',
+    width: '100%',
+    animationDuration: '3.7s',
+    animationIterationCount: 1,
+    animationTimingFunction: 'steps(60, end)',
+  },
+  'text p:nth-child(2)': {
+    animationName: 'type2',
+    animationDuration: '3.7s',
+  },
+  'text button': {
+    border: 0,
+    opacity: 0,
+    backgroundColor: '#191919',
+    color: '#f5d300',
+    borderWidth: 1,
+    letterSpacing: 2,
+    paddingVertical: 0.5,
+    paddingHorizontal: 2.5,
+    fontSize: 12,
+    fontWeight: '600',
+    textTransform: 'uppercase',
+    cursor: 'pointer',
+    transitionProperty: 'color, background-color',
+    transitionDuration: '0.5s',
+    animationDuration: '0.6s',
+    animationTimingFunction: 'ease-in-out',
+    animationIterationCount: 1,
+    animationFillMode: 'forwards',
+  },
+  'text button:hover': {
+    backgroundColor: '#f5d300',
+    color: '#191919',
+    borderWidth: 1,
+  },
+  splash: {
+    width: '100vw',
+    height: '100vh',
+    position: 'fixed',
+    top: 0,
+    display: 'flex',
+    overflow: 'hidden',
+  },
+  splash_logo: {
+    position: 'absolute',
+    margin: -15,
+    top: '50vh',
+    zIndex: 5,
+    left: '50vw',
+    width: 50,
+    textAlign: 'center',
+    height: 30,
+    fontSize: 26,
+    fontWeight: '600',
+    color: '#ffffff',
+    opacity: 1,
+    willChange: 'opacity',
+    animationDuration: '0.3s',
+    animationTimingFunction: 'ease-in',
+    animationIterationCount: 1,
+    animationFillMode: 'forwards',
+  },
+  'splash_logo:before': {
+    display: 'block',
+    position: 'absolute',
+    left: 15,
+    bottom: -5,
+    width: 20,
+    height: 1,
+    backgroundColor: '#757474',
+    content: '""',
+  },
+  'splash_logo:after': {
+    display: 'block',
+    position: 'absolute',
+    left: 15,
+    top: -5,
+    width: 20,
+    height: 1,
+    backgroundColor: '#757474',
+    content: '""',
+    willChange: 'width',
+  },
+  splash_svg: {
+    position: 'relative',
+    margin: 'auto',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    bottom: 0,
+    right: 0,
+  },
+  'splash_svg svg': {
+    width: '100%',
+    height: '100%',
+    backfaceVisibility: 'visible',
+  },
+  'splash_svg rect': {
+    width: '100%',
+    height: '100%',
+    fill: '#f5d300',
+    stroke: 0,
+    clipPath: 'polygon(45vw 40vh, 55vw 40vh, 55vw 60vh, 45vw 60vh)',
+    animationDuration: '0.7s',
+    animationTimingFunction: 'ease-in',
+    animationIterationCount: 1,
+    animationFillMode: 'forwards',
+  },
+  splash_minimize: {
+    position: 'absolute',
+    margin: 'auto',
+    width: '100%',
+    height: '100%',
+    top: 0,
+    left: 0,
+    zIndex: 4,
+  },
+  'splash_minimize svg': {
+    width: '100%',
+    height: '100%',
+    backfaceVisibility: 'visible',
+  },
+  'splash_minimize rect': {
+    width: '100%',
+    height: '100%',
+    clipPath: 'polygon(0vw 0vh, 100vw 0vh, 100vw 100vh, 0vw 100vh)',
+    animationDuration: '0.2s',
+    animationTimingFunction: 'ease-out',
+    animationIterationCount: 1,
+    animationFillMode: 'forwards',
+  },
+  '@keyframes scale': {
+    '100%': {
+      clipPath: 'polygon(45vw 40vh, 55vw 40vh, 55vw 60vh, 45vw 60vh)',
+    },
+  },
+  '@keyframes hide': {
+    '100%': {
+      fill: 'transparent',
+    },
+  },
+  '@keyframes off': {
+    '100%': {
+      opacity: 0,
+    },
+  },
+  '@keyframes on': {
+    '100%': {
+      opacity: 1,
+    },
+  },
+  '@keyframes logo': {
+    '100%': {
+      color: '#292929',
+    },
+  },
+  '@keyframes type': {
+    '0%': {
+      width: 0,
+    },
+  },
+  '@keyframes type2': {
+    '0%': {
+      width: 0,
+    },
+    '50%': {
+      width: 0,
+    },
+    '100%': {
+      width: 100,
+    },
+  },
+  '@keyframes expand': {
+    '25%': {
+      clipPath: 'polygon(0vw 0vh, 55vw 40vh, 55vw 60vh, 45vw 60vh)',
+      fill: 'white',
+    },
+    '50%': {
+      clipPath: 'polygon(0vw 0vh, 100vw 0vh, 55vw 60vh, 45vw 60vh)',
+      fill: '#f5d300',
+    },
+    '75%': {
+      clipPath: 'polygon(0vw 0vh, 100vw 0vh, 55vw 60vh, 0vw 100vh)',
+      fill: 'white',
+    },
+    '100%': {
+      clipPath: 'polygon(0vw 0vh, 100vw 0vh, 100vw 100vh, 0vw 100vh)',
+      fill: '#f5d300',
+    },
+  },
+});
+  
 export default Preloader;
